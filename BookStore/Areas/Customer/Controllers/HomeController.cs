2781ddd5ad4d4f36f8d25b201411d3f143ae7f1f
Anton <anthonylapin11@gmail.com>
2020-11-08 18:26:52 +0300

    Creating and configuring _ViewStart & _ViewImports

--- BookStore/Areas/Customer/Controllers/HomeController.cs
+++ BookStore/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BookStore.Models;
 
-namespace BookStore.Controllers
+namespace BookStore.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
